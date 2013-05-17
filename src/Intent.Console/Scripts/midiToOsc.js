ebf51feac246516f1ebaf704282a56ce788225f2
Michael Everett <michael.everett@gmail.com>
2013-05-17 05:07:59 -0600

    - Fix to midiToOsc.js from last checkin (was borked) - Removed old .js files

--- src/Intent.Console/Scripts/midiToOsc.js
+++ src/Intent.Console/Scripts/midiToOsc.js
@@ -1,4 +1,37 @@
-﻿// Create the MIDI to OSC adapter
+﻿//var count = 0;
+//var updateIntensity = 0;
+
+//var midiDate = function (type, channel, value1, value2) {
+//    var state = { channel: [], value: [] };
+
+//    for (var i = 0; i < lights.all.length; i++) {
+//        var light = lights.all[i];
+//        state.channel.push(light.dmx("r"));
+//        state.channel.push(light.dmx("g"));
+//        state.channel.push(light.dmx("b"));
+//        state.value.push(light.h + ";" + light.s + ";" + (light.v * updateIntensity));
+//    }
+
+//    return state;
+//};
+
+//var midiUpdate = function () {
+//    if (!midiToOsc.id) return;
+
+//    updateIntensity = (Math.sin(count) / 2) + 0.5;
+//    count += 0.1;
+
+//    sendMessage(midiToOsc.id, {
+//        type: "control change",
+//        channel: 1,
+//        value1: CC.v,
+//        value2: 0,
+//        address: "/lights",
+//        data: midiDate
+//    });
+//};
+
+// Create the MIDI to OSC adapter
 var midiToOsc = {
     // MIDI Input device
     device: "02. Internal MIDI",
@@ -19,7 +52,7 @@ var midiToOsc = {
     },
 
     // Update loop
-    update: midiUpdate
+    //update: midiUpdate
 };
 
 addAdapter(adapters.midiToOsc, midiToOsc);
